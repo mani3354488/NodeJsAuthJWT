d6d94113f0f6c1d9d48445852951fabc7f624ea1
mani3354488 <mani3354488@gmail.com>
2020-10-29 15:08:30 +0530

    setup express and did some error handling

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+idea to create a NodeJs Auth API - JWT - production ready ( best implementation for microsevices architecture)
---
+++ app.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const createError = require('http-errors')
+const { response } = require('express')
+
+const AuthRoute = require('./Routes/Auth.routh')
+
+const app = express()
+
+app.get('/', async (req, res, next) => {
+    res.send('Hello')
+})
+
+app.use('/auth', AuthRoute)
+
+app.use(async (req, res, next) => {
+    // const error = new Error("Not Found")
+    // error.status = 404
+    // next(error)
+    next(createError.NotFound('Not Found'))
+})
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+    res.send({
+        error: {
+            status: err.status || 500,
+            message: err.message,
+        }
+    })
+})
+
+const PORT = process.env.PORT || 3000 
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
